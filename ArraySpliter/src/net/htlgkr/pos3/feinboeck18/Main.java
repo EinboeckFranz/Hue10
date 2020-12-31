990eea8f432a2119ab8ee72af302ad255a1f3e2c
BuildTools <unconfigured@null.spigotmc.org>
2020-12-31 13:53:30 +0100

    Part 2 also finished - *Hue 10 ready to check*

--- ArraySpliter/src/net/htlgkr/pos3/feinboeck18/Main.java
+++ ArraySpliter/src/net/htlgkr/pos3/feinboeck18/Main.java
@@ -1,20 +1,51 @@
 package net.htlgkr.pos3.feinboeck18;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        Main main = new Main();
+        main.run();
     }
 
     public void run() {
-        for (int i = 0; i < 6; i++) {
+        String pathAsString = "numbers/numbers4";
+        Path path = new File(pathAsString).toPath();
+        int subArraySize;
+        List<Integer> numbers;
 
-        }
+        subArraySize = getSubarraySize(path);
+        numbers = getAllOtherNumbers(path);
+
+        Splitter splitter = new Splitter(subArraySize, numbers);
+        splitter.splitNumbers(5);
     }
 
-    private int[] getInputs(String path) {
+    private int getSubarraySize(Path path) {
+        try {
+            return Integer.parseInt(Files.lines(path).findFirst().orElse("0"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
+    private List<Integer> getAllOtherNumbers(Path path) {
+        List<Integer> numberCollector = new ArrayList<>();
+        try {
+            Files.lines(path)
+                    .skip(1)
+                    .forEach(line -> Arrays.stream(line.split(" "))
+                            .forEach(numberAsString -> numberCollector.add(Integer.parseInt(numberAsString))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return numberCollector;
     }
 }
--- ArraySpliter/src/net/htlgkr/pos3/feinboeck18/Splitter.java
+++ ArraySpliter/src/net/htlgkr/pos3/feinboeck18/Splitter.java
@@ -1,15 +1,86 @@
 package net.htlgkr.pos3.feinboeck18;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.*;
 
 public class Splitter {
-    public static List<int[]> splitIntoSubArrays(int subArrSize, int[] inputNumbs) {
-        List<int[]> tempSubArrays = new ArrayList<>();
-        if(inputNumbs.length == subArrSize) {
-            tempSubArrays.add(inputNumbs);
-            return tempSubArrays;
+    private final int subArrSize;
+    private final List<Integer> numbersToSplit;
+
+    public Splitter(int subArrSize, List<Integer> numbersToSplit) {
+        this.subArrSize = subArrSize;
+        this.numbersToSplit = numbersToSplit;
+    }
+
+    private static int applyAsInt(Future<Integer> returnedInt) {
+        try {
+            return returnedInt.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
-        
+        return 0;
+    }
+
+    public void splitNumbers(int threads) {
+        ExecutorService splitter = Executors.newFixedThreadPool(threads);
+        int max = 0;
+        int startWith = 0;
+        int spaceBetweenThreads = (numbersToSplit.size() / threads) - 1;
+        List<Callable<Integer>> callables = new ArrayList<>();
+
+        for (int i = 1; i <= threads; i++) {
+            if(i != threads) {
+                callables.add(new MaximumSearcher(numbersToSplit, subArrSize, startWith, startWith + spaceBetweenThreads));
+                startWith += spaceBetweenThreads + 1;
+            } else
+                callables.add(new MaximumSearcher(numbersToSplit, subArrSize, startWith, numbersToSplit.size() - 1));
+        }
+
+        try {
+            max = splitter.invokeAll(callables).stream()
+                    .mapToInt(Splitter::applyAsInt)
+                    .max()
+                    .orElse(0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            splitter.shutdown();
+        }
+
+        System.out.println(max);
+    }
+
+    private static class MaximumSearcher implements Callable<Integer> {
+        private final List<Integer> numbersToSplit;
+        private final int subArrSize;
+        private int currentValue;
+        private final int endValue;
+
+        public MaximumSearcher(List<Integer> numbersToSplit, int subArrSize, int starterValue, int endValue) {
+            this.numbersToSplit = numbersToSplit;
+            this.subArrSize = subArrSize;
+            this.currentValue = starterValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        public Integer call() {
+            int max = 0;
+            while(currentValue <= endValue) {
+                if(currentValue + subArrSize <= numbersToSplit.size()) {
+                    Set<Integer> setOfNumbers = new HashSet<>();
+                    for (int i = currentValue; i < currentValue + subArrSize; i++)
+                        setOfNumbers.add(numbersToSplit.get(i));
+                    if(setOfNumbers.size() > max)
+                        max = setOfNumbers.size();
+                } else
+                    break;
+                currentValue++;
+            }
+            return max;
+        }
     }
 }
